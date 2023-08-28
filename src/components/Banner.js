039fe74809fb5f4d59355dbb7867e7f7e91ab137
duy <dohoaduy0201@gmail.com>
2023-08-28 23:04:39 +0700

    add file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,47 @@
+import { useState, useEffect } from "react";
 import { Container, Col } from "react-bootstrap";
 import "./style.css"
 const Banner = () => {
+  
+    const [loopNum, setLoopNum] = useState(0);
+
+    const [text, setText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+
+    const period = 2000;
+    const toRotate = [ "Developer", "Gamer", "Freenlancer" ];
+    useEffect(() => {
+      let ticker = setInterval(() => {
+        tick();
+      }, delta);
+      return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  
+      setText(updatedText);
+      if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+      }
+  
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setIndex(prevIndex => prevIndex - 1);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setIndex(1);
+        setDelta(500);
+      } else {
+        setIndex(prevIndex => prevIndex + 1);
+      }
+    }
     return (  
         <section className="banner" id="home">
       <Container>
@@ -8,7 +49,7 @@ const Banner = () => {
             <div className="hello">Hello,</div>
             <div className="items">My name is</div>
             <div className="items" style={{color: 'red'}}>Do Hoa Duy</div>
-            <div className="items">I'm <span className="wrap">{`Developer`}</span></div>
+            <div className="items">I'm <span className="wrap" style={{color: "red"}}>{text}</span></div>
             <button onClick={() => console.log('connect')}>PORTPOLD</button>   
         </Col>
        
